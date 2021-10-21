e5968e01be89d4488e13591eacfdf33568b49586
Kirill Pshenichnyi <pshcyrill@mail.ru>
2021-10-21 12:11:42 +0200

    generate: add template

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ NP=8   # threads for compile
 ORIGIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 FFT3D_PATH=fft3d
+GENERATE_PATH=generate
 QAVER_PATH=qfft3daverage
 
 FFT3D_TMP=${FFT3D_PATH}/build
+GENERATE_TMP=${GENERATE_PATH}/build
 QAVER_TMP=${QAVER_PATH}/build
 
 BIN_PATH=bin
@@ -27,8 +29,17 @@ export CXX=${CXX}
 
 function Build() {
     # fft3d build
+    cd ${ORIGIN_DIR}
     mkdir -p ${FFT3D_TMP}
     cd ${FFT3D_TMP}
+    CC=${CC} CXX=${CXX} cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ../
+    mv compile_commands.json ../
+    make -j${NP} CC=${CC} CXX=${CXX}
+
+    # generate build
+    cd ${ORIGIN_DIR}
+    mkdir -p ${GENERATE_TMP}
+    cd ${GENERATE_TMP}
     CC=${CC} CXX=${CXX} cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ../
     mv compile_commands.json ../
     make -j${NP} CC=${CC} CXX=${CXX}
@@ -44,11 +55,13 @@ function Build() {
 
 function Clean(){
     cd ${ORIGIN_DIR}/${FFT3D_TMP} && make clean
+    cd ${ORIGIN_DIR}/${GENERATE_TMP} && make clean
     cd ${ORIGIN_DIR}/${QAVER_TMP} && make clean
     cd ${ORIGIN_DIR}
     rm -vrf ${BIN_PATH}
     rm -vrf ${LIB_PATH}
     rm -vrf ${FFT3D_TMP}
+    rm -vrf ${GENERATE_TMP}
     rm -vrf ${QAVER_TMP}
 }
 
